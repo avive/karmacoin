b2a0fb409609cc2ca484e6cf378ca76147bff7be
avive <aviveyal07@gmail.com>
2023-01-05 11:15:32 +0200

    refactor tests

---
+++ crates/server/tests/new_user_existing_number.rs
@@ -0,0 +1,32 @@
+// Copyright (c) 2022, KarmaCoin Authors. a@karmaco.in.
+// This work is licensed under the KarmaCoin v0.1.0 license published in the LICENSE file of this repo.
+//
+
+#[path = "common/mod.rs"]
+mod common;
+
+use common::{create_user, finalize_test, init_test};
+
+/// tests in this file should be run sequentially and not in parallel
+use server::server_service::{ServerService, Startup};
+use xactor::Service;
+
+// Attempt to create new user with an existing number
+#[tokio::test(flavor = "multi_thread")]
+async fn new_user_existing_number() {
+    init_test().await;
+
+    // Start the server
+    let server = ServerService::from_registry().await.unwrap();
+    server.call(Startup {}).await.unwrap().unwrap();
+
+    create_user("avive".into(), "972549805380".into())
+        .await
+        .unwrap();
+
+    create_user("angel".into(), "972549805380".into())
+        .await
+        .expect_err("should fail");
+
+    finalize_test().await;
+}
--- crates/server/tests/signup_referral.rs
+++ crates/server/tests/signup_referral.rs
@@ -26,7 +26,7 @@ use base::signed_trait::SignedTrait;
 use chrono::Utc;
 use prost::Message;
 
-mod new_user;
+mod new_user_happy_flow;
 mod payment_tx;
 
 use server::server_service::{ServerService, Startup};
