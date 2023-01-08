4e1819dbf4c47e6ef0be3f89222f63408cc7e4a8
avive <aviveyal07@gmail.com>
2023-01-08 23:15:09 +0200

    cleanup base proj

--- crates/base/lib.rs
+++
@@ -1,6 +0,0 @@
-// Copyright (c) 2022, KarmaCoin Authors.
-// This work is licensed under the KarmaCoin v0.1.0 license published in the LICENSE file of this repo.
-//
-
-extern crate serde;
-pub mod karma_coin;
