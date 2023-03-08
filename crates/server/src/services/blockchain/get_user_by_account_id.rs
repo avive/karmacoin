c577011efd5b4a921dcf804b444d221beccecc0d
avive <aviveyal07@gmail.com>
2023-03-08 19:19:44 +0200

    Add logging

--- crates/server/src/services/blockchain/get_user_by_account_id.rs
+++ crates/server/src/services/blockchain/get_user_by_account_id.rs
@@ -5,6 +5,7 @@
 use crate::services::blockchain::blockchain_service::BlockChainService;
 use crate::services::db_config_service::USERS_COL_FAMILY;
 use anyhow::{anyhow, Result};
+use base::hex_utils::short_hex_string;
 use base::karma_coin::karma_coin_api::{GetUserInfoByAccountRequest, GetUserInfoByAccountResponse};
 use base::karma_coin::karma_coin_core_types::User;
 use bytes::Bytes;
@@ -27,16 +28,28 @@ impl Handler<GetUserInfoByAccountId> for BlockChainService {
         };
 
         match DatabaseService::read(ReadItem {
-            key: Bytes::from(msg.0.account_id.unwrap().data),
+            key: Bytes::from(msg.0.account_id.as_ref().unwrap().data.clone()),
             cf: USERS_COL_FAMILY,
         })
         .await?
         {
             // fetch user by account id from db
-            Some(user_data) => Ok(GetUserInfoByAccountResponse {
-                user: Some(User::decode(user_data.0.as_ref())?),
-            }),
-            None => Ok(GetUserInfoByAccountResponse { user: None }),
+            Some(user_data) => {
+                info!(
+                    "found user on-chain by account id: {}",
+                    short_hex_string(user_data.0.as_ref())
+                );
+                Ok(GetUserInfoByAccountResponse {
+                    user: Some(User::decode(user_data.0.as_ref())?),
+                })
+            }
+            None => {
+                info!(
+                    "no user on-chain found account id: {}",
+                    short_hex_string(msg.0.account_id.unwrap().data.as_ref())
+                );
+                Ok(GetUserInfoByAccountResponse { user: None })
+            }
         }
     }
 }
